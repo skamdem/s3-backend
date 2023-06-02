5b49fe08fe4a654ed3b47708f83d24ebbc70b976
Sylvain K <sylvain.kamdem@gmail.com>
2023-06-02 10:05:07 +0100

    reorganized files

--- variables.tf
+++ variables.tf
@@ -1,9 +1,6 @@
+#  variables definition
+
 variable "region" {
   default = "us-east-1"
 }
 
-# variable "tags" {
-#   type        = map(any)
-#   default     = {}
-#   description = "Additional tags (e.g. `map('BusinessUnit`,`XYZ`)"
-# }
